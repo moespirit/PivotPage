cc55e86d76a2ae781263f9c2ce193ac20b04eaf0
yinyue200 <yinyue200@outlook.com>
2017-07-25 20:59:09 +0800

    add new demo

---
+++ PivotPageNewDemo/PivotPageNewDemo.Android/MainActivity.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace PivotPageNewDemo.Droid
+{
+	[Activity (Label = "PivotPageNewDemo", Icon = "@drawable/icon", Theme="@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+	{
+		protected override void OnCreate (Bundle bundle)
+		{
+			TabLayoutResource = Resource.Layout.Tabbar;
+			ToolbarResource = Resource.Layout.Toolbar; 
+
+			base.OnCreate (bundle);
+
+			global::Xamarin.Forms.Forms.Init (this, bundle);
+			LoadApplication (new PivotPageNewDemo.App ());
+
+            AndroidEnvironment.UnhandledExceptionRaiser += AndroidEnvironment_UnhandledExceptionRaiser;
+
+        }
+        private void AndroidEnvironment_UnhandledExceptionRaiser(object sender, RaiseThrowableEventArgs e)
+        {
+#if DEBUG
+            if(System.Diagnostics.Debugger.IsAttached)
+            {
+                System.Diagnostics.Debug.WriteLine(e.Exception);
+                System.Diagnostics.Debugger.Break();
+            }
+#endif
+        }
+    }
+}
+
---
+++ PivotPageNewDemo/PivotPageNewDemo/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Yinyue200.Controls.PivotPage;
+
+namespace PivotPageNewDemo
+{
+	public partial class MainPage : PivotPage
+    {
+		public MainPage()
+		{
+			InitializeComponent();
+            Headers = new string[] { "1", "2" };
+            Views = Views.ToArray();//暴力方法强行让内部发现属性值已经变化
+        }
+	}
+    public static class PivotPageHelper
+    {
+        public static void Dispose(this Yinyue200.Controls.PivotPage.PivotPage page)
+        {
+#if ANDROID
+            foreach(var one in page.Views)
+            {
+                var renderer = Platform.GetRenderer(one);
+                if (renderer != null)
+                {
+                    renderer.View.RemoveFromParent();
+                }
+            }
+#endif
+        }
+    }
+}
